5d773ea55bb4782f3710a89af9b222659057a3c3
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2016-08-31 12:48:47 +0100

    koans-1 passing

--- koans-1/tasks/1-React-TodoList-props-and-state.js
+++ koans-1/tasks/1-React-TodoList-props-and-state.js
@@ -1,42 +1,22 @@
 import React from "react";
 
-// Task: Fill the `return` of `TodoList` class. 
-// It should return a list of `ListItem`. You need to display the task name.
-
-// Minimize the state in your components and get data using props from parent components whenever possible.
-// In the example ListItems shouldn't have state
 class TodoList extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      tasks: [ { name: "Learn more JavaScript" },
-               { name: "Learn more testing too!" }
-             ]
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			tasks: [
+				{ name: "Learn more JavaScript" },
+				{ name: "Learn more testing too!" }
+			]
+		};
+	}
 
-  render() {
-    // Properties can pass data to React components. The sintax is similar to HTML elements' attributs.
-    // Below you can see how to pass data to a child component using a 'task' property
-    let tasksComponents = this.state.tasks.map( task => (
-        <ListItem />)
-    )
-
-    // Hint: Don't forget to wrap it with an `ul` element
-    return (
-      <div>
-          // Write some code here
-      </div>
-    );
-  }
+	render() {
+		let tasksComponents = this.state.tasks.map( task => <ListItem task={task} />);
+		return <ul>{tasksComponents}</ul>;
+	}
 }
 
-// Render task name from the component's properties.
-// Don't you know how?  Some key words: interpolate react props
-export const ListItem = props =>(
-    <li>
-          // Write some code here
-    </li>
-);
+export const ListItem = props => <li>{props.task.name}</li>;
 
 export default TodoList;
